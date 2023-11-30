7e40fa282506131832f514c04ccf03f631552b5a
Hallex <136874674+Hallexz@users.noreply.github.com>
2023-11-30 13:35:41 +0700

    Update Program.cs

--- Calcul/WindowsFormsApp3/Program.cs
+++ Calcul/WindowsFormsApp3/Program.cs
@@ -1,15 +1,15 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsApp3
+namespace Calculator
 {
     static class Program
     {
         /// <summary>
-        /// Главная точка входа для приложения.
+        /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
